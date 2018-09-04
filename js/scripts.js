c766177432bac5e63bfc4417474c4dad242d0968
Ron Craig <ron.craig@comcast.net>
2018-09-04 00:56:07 -0700

    edit html file, add second toggle flashcard

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".showing").toggle();
-    $(".hidden").toggle();
+  $(".clickable-js").click(function() {
+    $(".showing-js").toggle();
+    $(".hidden-js").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable-equal").click(function() {
+    $(".showing-equal").toggle();
+    $(".hidden-equal").toggle();
   });
 });
